be8a1de44f9c91c39da48e1350c0061aebbf3324
G1A021037-ANJASFEDO <fedoafridiansah@gmail.com>
2023-10-02 08:21:17 +0700

    Minor Problem

--- config/app.php
+++ config/app.php
@@ -168,8 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
         Spatie\Permission\PermissionServiceProvider::class,
+    ])->toArray(),
 
     /*
     |--------------------------------------------------------------------------
